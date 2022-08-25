4cabca3bf5698a87f5a45b7a0c0831b548c31b04
miligar <emilguiga@gmail.com>
2022-08-25 14:15:44 +0100

    added messages and heal function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This Monster Slayer Mini Game has been made using VueJS
 
 Play it on:
+
+https://miligar.github.io/The-Monster-Slayer-Game-With-Vue/
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
    playerHealth: 100,
    monsterHealth: 100,
    currentRound: 0,
+   winner: null,
   };
  },
  computed: {
@@ -19,6 +20,22 @@ const app = Vue.createApp({
   },
   mayUseSpecialAttack() {
    return this.currentRound % 3 !== 0;
+  },
+ },
+ watch: {
+  playerHealth(value) {
+   if (value <= 0 && this.monsterHealth <= 0) {
+    this.winner = "draw";
+   } else if (value <= 0) {
+    this.winner = "monster";
+   }
+  },
+  monsterHealth() {
+   if (value <= 0 && this.playerHealth <= 0) {
+    this.winner = "draw";
+   } else if (value <= 0) {
+    this.winner = "player";
+   }
   },
  },
  methods: {
@@ -36,6 +53,16 @@ const app = Vue.createApp({
    this.currentRound++;
    const attackValue = getRandomValue(25, 10);
    this.monsterHealth -= attackValue;
+   this.attackPlayer();
+  },
+  healPlayer() {
+   this.currentRound++;
+   const healValue = getRandomValue(8, 20);
+   if (this.playerHealth + healValue > 100) {
+    this.playerHealth = 100;
+   } else {
+    this.playerHealth += healValue;
+   }
    this.attackPlayer();
   },
  },
